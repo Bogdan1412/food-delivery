4ff77f96da5a517f841832062cb093c955b5c942
Bohdan Savchuk <98241166+Bogdan1412@users.noreply.github.com>
2025-01-23 06:51:32 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ import cards from './modules/cards';
 import { openModal } from './modules/modal'
 
 document.addEventListener('DOMContentLoaded', () => {
-	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000)
+	const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 5000)
 
 	tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active')
 	calc()
